65739ef047233567e6053fc57464a42ffe69e989
Peter Hutterer <peter.hutterer@who-t.net>
2015-03-25 13:37:09 +1000

    linux: bring our process group to the foreground (#89653)
    Required to make the driver receive keyboard events from the console.
    
    X.Org Bug  <http://bugs.freedesktop.org/show_bug.cgi?id=89653>
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Tested-by: Ingo Brückl <ib@wupperonline.de>

--- src/lnx_kbd.c
+++ src/lnx_kbd.c
@@ -187,8 +187,17 @@ OpenKeyboard(InputInfoPtr pInfo)
 
     s = xf86SetStrOption(pInfo->options, "Device", NULL);
     if (s == NULL) {
+       int rc;
        pInfo->fd = xf86Info.consoleFd;
        pKbd->isConsole = TRUE;
+
+       rc = tcsetpgrp(pInfo->fd, getpgid(0));
+       if (rc < 0) {
+           xf86IDrvMsg(pInfo, X_ERROR,
+                       "failed to set us as foreground pgrp (%s)\n",
+                       strerror(errno));
+       }
+
     } else {
        pInfo->fd = open(s, O_RDONLY | O_NONBLOCK | O_EXCL);
        if (pInfo->fd == -1) {
